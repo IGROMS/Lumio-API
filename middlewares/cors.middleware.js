c0cf159f434b8c4570cfdf717186b44074cbc91d
Ignacio Romaní <ignacioromani@gmail.com>
2022-11-11 00:05:44 +0100

    va

--- middlewares/cors.middleware.js
+++ middlewares/cors.middleware.js
@@ -1,5 +1,5 @@
 app.use((req, res, next) => {
-    res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.set("Access-Control-Allow-Origin", "https://lumio-sage.vercel.app/");
     res.set("Access-Control-Allow-Headers", "content-type");
     res.set("Access-Control-Allow-Methods", "*");
     res.set("Access-Control-Allow-Credentials", "true");
