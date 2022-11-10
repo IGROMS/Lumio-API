cf91067064806905de40a81d70aa2976b2ba8338
Ignacio Romaní <ignacioromani@gmail.com>
2022-11-10 22:06:33 +0100

    test

--- config/db.config.js
+++ config/db.config.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose')
 const DB_NAME = 'Lumio';
 const URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
 
-
+console.log(URI)
 mongoose
   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.info(`Successfully connected to the database ${DB_URI}`))
+  .then(() => console.info(`Successfully connected to the database`))
   .catch((error) => {
-    console.error(`An error ocurred trying to connect to de database ${DB_URI}`, error);
+    console.error(`An error ocurred trying to connect to de database`, error);
     process.exit(0);
   });
 
